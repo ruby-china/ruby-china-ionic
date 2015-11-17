936243c24c2d9e9a5176b87fc8b85e08c61d45bc
Madz <dothide@gmail.com>
2015-11-17 11:52:54 +0800

    完成程序主要结构框架搭建

---
+++ www/js/controllers/main.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.controller')
+    .controller('MainController', MainController);
+
+  ////////////////////////////////////////////////////////////
+
+  /* @ngInject */
+  function MainController() {
+
+  }
+
+})();
---
+++ www/js/controllers/topics.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.controller')
+    .controller('TopicsController', TopicsController);
+
+  ////////////////////////////////////////////////////////////
+
+  /* @ngInject */
+  function TopicsController() {
+
+  }
+
+})();
