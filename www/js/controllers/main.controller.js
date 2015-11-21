62fc38dc8648e64f66d64415874fde84b8997deb
Madz <dothide@gmail.com>
2015-11-21 16:16:19 +0800

    调整发布新话题界面

--- www/js/controllers/main.controller.js
+++ www/js/controllers/main.controller.js
@@ -8,8 +8,10 @@
   ////////////////////////////////////////////////////////////
 
   /* @ngInject */
-  function MainController($scope, ionicMaterialInk, ionicMaterialMotion, $ionicActionSheet,
-    $ionicSideMenuDelegate, $timeout, BaseService, AuthService, CameraService, TopicService) {
+  function MainController($rootScope, $scope, ionicMaterialInk,
+    ionicMaterialMotion, $ionicScrollDelegate,
+    $timeout, BaseService, AuthService,
+    CameraService, TopicService) {
 
     var vm = this;
     vm.current_user = {};
@@ -18,7 +20,6 @@
     vm.errorMsg = "";
     vm.nodes = [];
     vm.new_topic = {};
-    vm.bar_color = "red";
 
     // Functions
     vm.showLoginModal = showLoginModal;
@@ -27,8 +28,14 @@
     vm.logout = logout;
     vm.showNewTopicModal = showNewTopicModal;
     vm.closeNewTopicModal = closeNewTopicModal;
-    vm.moreAction = moreAction;
+    // vm.moreAction = moreAction;
     vm.createTopic = createTopic;
+
+    vm.imageByCamera = imageByCamera;
+    vm.imageByGallery = imageByGallery;
+    vm.insertCode = insertCode;
+    vm.atSomebody = atSomebody;
+    vm.insertEmoji = insertEmoji;
 
     activate();
 
@@ -38,8 +45,12 @@
         ionicMaterialMotion.ripple();
       }, 0);
       loadData("");
-      BaseService.registModal('modals/login.html', 'login-modal', $scope);
-      BaseService.registModal('modals/new_topic.html', 'new-topic-modal', $scope);
+      BaseService.registModal('modals/login.html', 'login-modal', $scope, {
+        focusFirstInput: true
+      });
+      BaseService.registModal('modals/new_topic.html', 'new-topic-modal', $scope, {
+        focusFirstInput: true
+      });
     }
 
     function loadData(err) {
@@ -81,46 +92,65 @@
           TopicService.getAllNodes()
             .then(function(result) {
               vm.nodes = result.nodes;
+              setTextareaHeight(0).textareaAutoSize();
+
+              // 键盘弹出后，把底部操作条顶上来
+              window.addEventListener('native.keyboardshow', function(event) {
+                if ($("textarea[id=new-topic]:focus").length > 0) {
+                  moreActionPutup(event.keyboardHeight);
+                };
+              });
+
+              // 光标离开内容区，操作条回去
+              $("#new-topic").on('blur', function() {
+                moreActionDown();
+              });
+
               return vm.nodes;
             });
         });
     }
 
     function closeNewTopicModal() {
-      BaseService.hideModal('new-topic-modal');
+      BaseService.hideModal('new-topic-modal')
+        .then(function() {
+          vm.new_topic = {};
+          $('#new-topic').css("max-height", "31px");
+        });
     }
 
-    function moreAction() {
-      var options = {
-        buttons: [{
-          text: '<i class="mdi mdi-image-area"></i> 从相册添加图片'
-        }, {
-          text: '<i class="mdi mdi-camera"></i> 从相机添加图片'
-        }],
-        titleText: '更多',
-        cancelText: '取消',
-        buttonClicked: function(index) {
-          document.addEventListener("deviceready", function() {
-            var size = {
-              width: 800,
-              height: 600
-            };
-            return CameraService.getPicture(index, size, 0)
-              .then(function(result) {
-                BaseService.uploadPicture(result)
-                  .then(function(img) {
-                    var img_url = '![](' + img.image_url + ')';
-                    var prev = vm.new_topic.body.length === 0 ? '' : vm.new_topic.body + "\r\n";
-                    vm.new_topic.body = prev + img_url;
-                  });
-              });
-          }, false);
+    // 界面调整 2015-11-20
+    // function moreAction() {
+    //   var options = {
+    //     buttons: [{
+    //       text: '<i class="mdi mdi-image-area"></i> 从相册添加图片'
+    //     }, {
+    //       text: '<i class="mdi mdi-camera"></i> 从相机添加图片'
+    //     }],
+    //     titleText: '更多',
+    //     cancelText: '取消',
+    //     buttonClicked: function(index) {
+    //       document.addEventListener("deviceready", function() {
+    //         var size = {
+    //           width: 600,
+    //           height: 600
+    //         };
+    //         return CameraService.getPicture(index, size, 0)
+    //           .then(function(result) {
+    //             BaseService.uploadPicture(result)
+    //               .then(function(img) {
+    //                 var img_url = '![](' + img.image_url + ')';
+    //                 var prev = vm.new_topic.body.length === 0 ? '' : vm.new_topic.body + "\r\n";
+    //                 vm.new_topic.body = prev + img_url;
+    //               });
+    //           });
+    //       }, false);
 
-          return true;
-        }
-      }
-      return $ionicActionSheet.show(options);
-    }
+    //       return true;
+    //     }
+    //   }
+    //   return $ionicActionSheet.show(options);
+    // }
 
     // 发表新话题
     function createTopic() {
@@ -135,6 +165,83 @@
           BaseService.hideLoading();
           BaseService.alert('发表新话题', '提交失败', err.error);
         })
+    }
+
+    // 来自相机的照片
+    function imageByCamera() {
+      moreActionDown();
+      document.addEventListener("deviceready", function() {
+        var size = {
+          width: 600,
+          height: 600
+        };
+        return CameraService.getPicture(1, size, 0)
+          .then(function(result) {
+            BaseService.uploadPicture(result)
+              .then(function(img) {
+                var img_url = '![](' + img.image_url + ')';
+                var prev = vm.new_topic.body.length === 0 ? '' : vm.new_topic.body + "\r\n";
+                vm.new_topic.body = prev + img_url;
+              });
+          });
+      }, false);
+    }
+
+    // 来自相册的照片
+    function imageByGallery() {
+      moreActionDown();
+      document.addEventListener("deviceready", function() {
+        var size = {
+          width: 600,
+          height: 600
+        };
+        return CameraService.getPicture(0, size, 0)
+          .then(function(result) {
+            BaseService.uploadPicture(result)
+              .then(function(img) {
+                var img_url = '![](' + img.image_url + ')';
+                var prev = vm.new_topic.body.length === 0 ? '' : vm.new_topic.body + "\r\n";
+                vm.new_topic.body = prev + img_url;
+              });
+          });
+      }, false);
+    }
+
+    // 插入代码
+    function insertCode() {
+      moreActionDown();
+    }
+
+    // @某人
+    function atSomebody() {
+      moreActionDown();
+    }
+
+    // 插入表情
+    function insertEmoji() {
+      moreActionDown();
+    }
+
+    // 处理操作条的方法不太完美，后期改进
+    // 操作条提上来
+    function moreActionPutup(height) {
+      $('#more-actions').css("transform", "translate(0px, -" + height + "px)");
+      setTextareaHeight(height)
+        .scrollTop($('#new-topic')[0].scrollHeight);
+    }
+
+    // 操作条回到底部
+    function moreActionDown() {
+      $('#more-actions').css("transform", "translate(0px, 0px)");
+      setTextareaHeight(0);
+    }
+
+    function setTextareaHeight(height) {
+      vm.textarea_max_height =
+        document.documentElement.clientHeight - $('.bar-header').height() - 96 - 40;
+      $('#new-topic').css("max-height", vm.textarea_max_height - height)
+        .height(vm.textarea_max_height);
+      return $('#new-topic');
     }
   }
 
--- www/js/core/run.core.js
+++ www/js/core/run.core.js
@@ -14,7 +14,7 @@
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
       }
       if (window.StatusBar) {
