055cf561f42c2fc93fe8b3a7b1606c08bff29431
Jason Lee <huacnlee@gmail.com>
2016-01-05 14:31:47 +0800

    Fix iOS build

--- hooks/after_prepare/040_move_dist_files_to_platforms.js
+++ hooks/after_prepare/040_move_dist_files_to_platforms.js
@@ -9,11 +9,11 @@ var mv = require('mv');
 var platform = process.env.CORDOVA_PLATFORMS;
 
 function moveIosFolder() {
-  var iosPlatformsDir_dist_css = path.resolve(__dirname, '../../platforms/ios/www/dist/dist_css');
-  var iosPlatformsDir_dist_js = path.resolve(__dirname, '../../platforms/ios/www/dist/dist_js');
-  var iosPlatformsDir_dist_index = path.resolve(__dirname, '../../platforms/ios/www/dist/index.html');
-  var iosPlatformsDir_www_css = path.resolve(__dirname, '../../platforms/ios/www/dist_css');
-  var iosPlatformsDir_www_js = path.resolve(__dirname, '../../platforms/ios/www/dist_js');
+  var iosPlatformsDir_dist_css = path.resolve(__dirname, '../../platforms/ios/www/css');
+  var iosPlatformsDir_annotated_js = path.resolve(__dirname, '../../platforms/ios/www/annotated_js');
+  var iosPlatformsDir_dist_index = path.resolve(__dirname, '../../platforms/ios/www/index.html');
+  var iosPlatformsDir_www_css = path.resolve(__dirname, '../../platforms/ios/www/css');
+  var iosPlatformsDir_www_js = path.resolve(__dirname, '../../platforms/ios/www/annotated_js');
   var iosPlatformsDir_www_index = path.resolve(__dirname, '../../platforms/ios/www/index.html');
   console.log("Moving dist files to iOS platform");
   mv(iosPlatformsDir_dist_css, iosPlatformsDir_www_css, {
@@ -28,7 +28,7 @@ function moveIosFolder() {
     }
   });
 
-  mv(iosPlatformsDir_dist_js, iosPlatformsDir_www_js, {
+  mv(iosPlatformsDir_annotated_js, iosPlatformsDir_www_js, {
     mkdirp: true
   }, function(err) {
     if (typeof err != 'undefined') {
@@ -53,10 +53,10 @@ function moveIosFolder() {
 
 function moveAndroidFolder() {
   var androidPlatformsDir_dist_css = path.resolve(__dirname, '../../platforms/android/assets/www/dist/dist_css');
-  var androidPlatformsDir_dist_js = path.resolve(__dirname, '../../platforms/android/assets/www/dist/dist_js');
+  var androidPlatformsDir_annotated_js = path.resolve(__dirname, '../../platforms/android/assets/www/dist/annotated_js');
   var androidPlatformsDir_dist_index = path.resolve(__dirname, '../../platforms/android/assets/www/dist/index.html');
   var androidPlatformsDir_www_css = path.resolve(__dirname, '../../platforms/android/assets/www/dist_css');
-  var androidPlatformsDir_www_js = path.resolve(__dirname, '../../platforms/android/assets/www/dist_js');
+  var androidPlatformsDir_www_js = path.resolve(__dirname, '../../platforms/android/assets/www/annotated_js');
   var androidPlatformsDir_www_index = path.resolve(__dirname, '../../platforms/android/assets/www/index.html');
 
   console.log("Moving dist files to Android platform");
@@ -73,7 +73,7 @@ function moveAndroidFolder() {
 
   });
 
-  mv(androidPlatformsDir_dist_js, androidPlatformsDir_www_js, {
+  mv(androidPlatformsDir_annotated_js, androidPlatformsDir_www_js, {
     mkdirp: true
   }, function(err) {
     if (typeof err != 'undefined') {
