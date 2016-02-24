ab923b852ccdd4584939a5638e4e59eb94957355
Madz <dothide@gmail.com>
2016-02-24 11:35:56 +0800

    调整文件结构

--- app/pages/topics/topics.page.ts
+++ app/pages/topics/topics.page.ts
@@ -1,11 +1,10 @@
 import {CORE_DIRECTIVES} from "angular2/common";
 import {Page} from 'ionic-framework/ionic';
-import {Topic} from "../components/topic/components/topic";
-import {TopicService} from "../components/topic/services/topic.service";
+import {TopicService} from "../../services/topic.service";
 
 @Page({
   templateUrl: "build/pages/topics/topics.page.html",
-  directives: [Topic, CORE_DIRECTIVES],
+  directives: [CORE_DIRECTIVES],
   providers: [TopicService]
 })
 export class TopicsPage {
